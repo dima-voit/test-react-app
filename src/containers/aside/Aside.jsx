e6da1ee0e1de26e2c1ddcce8f1ee95f013f9a42d
Dima Voit <dmitriy.voyt@gmail.com>
2022-01-27 00:23:20 +0200

    added base structure files

---
+++ src/containers/aside/Aside.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './aside.css';
+
+const Aside = () => {
+  return (
+    <div>
+      Aside
+    </div>
+  )
+}
+
+export default Aside
---
+++ src/containers/index.js
@@ -0,0 +1,3 @@
+export { default as Aside } from './aside/Aside';
+export { default as Footer } from './footer/Footer';
+export { default as Header } from './header/Header';
